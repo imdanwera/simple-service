39f411d5323848d054fc3f018833ab3b16a14a93
imdanwera <im.danwera@gmail.com>
2020-10-27 13:28:57 +0530

    first commit

---
+++ src/main/java/com/example/simple/controllers/AppController.java
@@ -0,0 +1,15 @@
+package com.example.simple.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/app")
+public class AppController {
+
+    @GetMapping("/hi")
+    public String sayHello(){
+        return "Hello";
+    }
+}
