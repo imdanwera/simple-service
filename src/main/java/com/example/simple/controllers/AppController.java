0ae774cd9803b588479f8890006785bcff701811
imdanwera <im.danwera@gmail.com>
2021-08-05 09:05:17 +0530

    added ec2 log path

--- src/main/java/com/example/simple/controllers/AppController.java
+++ src/main/java/com/example/simple/controllers/AppController.java
@@ -1,16 +1,23 @@
 package com.example.simple.controllers;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
 
 @RestController
 @RequestMapping("/api/app")
 public class AppController {
 
+    Logger logger = LoggerFactory.getLogger(AppController.class);
+
     @GetMapping("/hi")
     public String sayHello(){
+        logger.info("INFO: WelCome!");
         System.out.println("Hello");
+        logger.info("INFO: Bye !!!");
         return "Hello\n";
+
     }
 }
